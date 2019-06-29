959f8507e4a585455c2a7debd19702d9d2e7f4d2
galihsatriawan <satriawan.galih99@gmail.com>
2019-06-29 20:39:44 +0700

    - ListView - ArrayAdapter

--- MyListView/app/src/main/java/id/shobrun/mylistview/MainActivity.java
+++ MyListView/app/src/main/java/id/shobrun/mylistview/MainActivity.java
@@ -2,12 +2,20 @@ package id.shobrun.mylistview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView lvList ;
+    private String[] dataName = {"Cut Nyak Dien","Ki Hajar Dewantara"
+            ,"Moh Yamin", "Patimura","R A Kartini","Sukarno"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lvList = findViewById(R.id.lv_list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>
+                (this,android.R.layout.simple_list_item_1,android.R.id.text1,dataName);
+        lvList.setAdapter(adapter);
     }
 }
