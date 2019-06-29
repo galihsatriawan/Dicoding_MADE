158d3291cc207d9d81a0a38263de8113db7558a5
galihsatriawan <satriawan.galih99@gmail.com>
2019-06-29 16:00:36 +0700

    - Fragment - Flexible Fragment - Two Fragment

---
+++ MyFlexibleFragment/app/src/main/java/id/shobrun/myflexiblefragment/fragments/CategoryFragment.java
@@ -0,0 +1,49 @@
+package id.shobrun.myflexiblefragment.fragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import id.shobrun.myflexiblefragment.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CategoryFragment extends Fragment implements View.OnClickListener {
+
+
+    public CategoryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_category, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button btnDetailCategory = view.findViewById(R.id.btn_detail_category);
+        btnDetailCategory.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_detail_category:{
+
+                break;
+            }
+        }
+    }
+}
--- MyFlexibleFragment/app/src/main/java/id/shobrun/myflexiblefragment/fragments/HomeFragment.java
+++ MyFlexibleFragment/app/src/main/java/id/shobrun/myflexiblefragment/fragments/HomeFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -39,6 +41,20 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_category : {
+                FragmentManager mFragmentManager = getFragmentManager();
+                if(mFragmentManager != null){
+                    CategoryFragment mCategoryFragment = new CategoryFragment();
+                    FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
+                    mFragmentTransaction.replace(R.id.frame_container,mCategoryFragment,CategoryFragment.class.getSimpleName());
+                    // Add to stack
+                    mFragmentTransaction.addToBackStack(null);
+                    mFragmentTransaction.commit();
+                }
 
+                break;
+            }
+        }
     }
 }
