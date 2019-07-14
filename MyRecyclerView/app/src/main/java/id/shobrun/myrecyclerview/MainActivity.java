7837a36d3d718930329c499c9059665991ba3d63
galihsatriawan <satriawan.galih99@gmail.com>
2019-07-14 10:13:17 +0700

    - RecyclerView - Complete CardView with SaveInstance

--- MyRecyclerView/app/src/main/java/id/shobrun/myrecyclerview/MainActivity.java
+++ MyRecyclerView/app/src/main/java/id/shobrun/myrecyclerview/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 
 import java.util.ArrayList;
 
+import id.shobrun.myrecyclerview.adapter.CardHeroAdapter;
 import id.shobrun.myrecyclerview.adapter.GridHeroAdapter;
 import id.shobrun.myrecyclerview.adapter.ListHeroAdapter;
 import id.shobrun.myrecyclerview.data.HeroesData;
@@ -18,14 +19,34 @@ import id.shobrun.myrecyclerview.pojo.Hero;
 public class MainActivity extends AppCompatActivity {
     RecyclerView rvHeroes;
     ArrayList<Hero> datas = new ArrayList<>();
+    public final String EXTRA_TITLE ="extra_title";
+    public final String EXTRA_LIST ="extra_list";
+    public final String EXTRA_MODE = "extra_mode";
+    private String title = "Mode List";
+    private int mode = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         rvHeroes = findViewById(R.id.recyler_hero);
         rvHeroes.setHasFixedSize(true);
-        prepare();
-        showRecyclerList();
+        if(savedInstanceState == null){
+            prepare();
+            showRecyclerList();
+            mode = R.id.action_list;
+        }else{
+            ArrayList<Hero> stateList = savedInstanceState.getParcelableArrayList(EXTRA_LIST);
+            mode = savedInstanceState.getInt(EXTRA_MODE);
+
+            if(stateList!= null){
+                datas.addAll(stateList);
+            }
+            setMode(mode);
+
+        }
+
+
 
     }
     public void prepare(){
@@ -41,7 +62,11 @@ public class MainActivity extends AppCompatActivity {
         GridHeroAdapter adapter = new GridHeroAdapter(datas);
         rvHeroes.setAdapter(adapter);
     }
-
+    public void showRecylcerCard(){
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        CardHeroAdapter adapter = new CardHeroAdapter(datas);
+        rvHeroes.setAdapter(adapter);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main,menu);
@@ -56,17 +81,35 @@ public class MainActivity extends AppCompatActivity {
     void setMode(int selectedMode){
         switch (selectedMode){
             case R.id.action_list:{
+                title = "Mode List";
                 showRecyclerList();
                 break;
             }
             case R.id.action_grid:{
+                title = "Mode Grid";
                 showRecyclerGrid();
                 break;
             }
             case R.id.action_cardview:{
-
+                title = "Mode CardView";
+                showRecylcerCard();
                 break;
             }
         }
+        mode = selectedMode;
+        setActionBarTitle(title);
+    }
+    public void setActionBarTitle(String title){
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setTitle(title);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(EXTRA_TITLE,title);
+        outState.putParcelableArrayList(EXTRA_LIST,datas);
+        outState.putInt(EXTRA_MODE,mode);
     }
 }
---
+++ MyRecyclerView/app/src/main/java/id/shobrun/myrecyclerview/adapter/CardHeroAdapter.java
@@ -0,0 +1,78 @@
+package id.shobrun.myrecyclerview.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+
+import id.shobrun.myrecyclerview.R;
+import id.shobrun.myrecyclerview.pojo.Hero;
+
+public class CardHeroAdapter extends RecyclerView.Adapter<CardHeroAdapter.CardViewHolder> {
+    private ArrayList<Hero> heroes;
+
+    public CardHeroAdapter(ArrayList<Hero> heroes) {
+        this.heroes = heroes;
+    }
+
+    @NonNull
+    @Override
+    public CardViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_cardview_hero,viewGroup,false);
+
+        return new CardViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CardViewHolder holder, int i) {
+        final Hero hero = heroes.get(i);
+        Glide.with(holder.itemView.getContext())
+                .load(hero.getPhoto())
+                .apply(new RequestOptions().override(350,550))
+                .into(holder.imgPhotos);
+
+        holder.tvName.setText(hero.getName());
+        holder.tvFrom.setText(hero.getFrom());
+        holder.btnShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(),"Share "+hero.getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
+        holder.btnFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(),"Favorite "+hero.getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return heroes.size();
+    }
+
+    public class CardViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgPhotos;
+        TextView tvName,tvFrom;
+        Button btnShare,btnFavorite;
+        public CardViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgPhotos = itemView.findViewById(R.id.image_item_photo);
+            tvName = itemView.findViewById(R.id.text_item_name);
+            tvFrom = itemView.findViewById(R.id.text_item_from);
+            btnShare = itemView.findViewById(R.id.button_set_share);
+            btnFavorite = itemView.findViewById(R.id.button_set_favorite);
+        }
+    }
+}
