50d21bc3f4fb1afafb72253a04f26f5eefe5707f
galihsatriawan <satriawan.galih99@gmail.com>
2019-07-15 09:15:39 +0700

    - Action Bar - Make First Action Bar With Icon and overflow

--- MyActionBar/app/src/main/java/id/shobrun/myactionbar/MainActivity.java
+++ MyActionBar/app/src/main/java/id/shobrun/myactionbar/MainActivity.java
@@ -1,7 +1,15 @@
 package id.shobrun.myactionbar;
 
+import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import id.shobrun.myactionbar.fragment.MenuFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu1 :
+                MenuFragment menuFragment = new MenuFragment();
+                FragmentManager mFragmentManager = getSupportFragmentManager();
+                FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
+                mFragmentTransaction.replace(R.id.fragment_container,menuFragment);
+                mFragmentTransaction.addToBackStack(null);
+                mFragmentTransaction.commit();
+
+                break;
+            case R.id.menu2:
+                Intent i  = new Intent(this, MenuActivity.class);
+                startActivity(i);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ MyActionBar/app/src/main/java/id/shobrun/myactionbar/MenuActivity.java
@@ -0,0 +1,21 @@
+package id.shobrun.myactionbar;
+
+import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import id.shobrun.myactionbar.fragment.MenuFragment;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
