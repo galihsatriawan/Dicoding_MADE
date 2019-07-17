5fe37dfa053ab0a6d40751249582da97ce30dea8
galihsatriawan <satriawan.galih99@gmail.com>
2019-07-17 20:34:21 +0700

    - Bottom Navigation - Complete Bottom Navigation

--- MyBottomNavigation/app/src/main/java/id/shobrun/mybottomnavigation/MainActivity.java
+++ MyBottomNavigation/app/src/main/java/id/shobrun/mybottomnavigation/MainActivity.java
@@ -2,28 +2,42 @@ package id.shobrun.mybottomnavigation;
 
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView mTextMessage;
 
+    public String title;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment ;
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    fragment = new HomeFragment();
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container_layout,fragment)
+                            .commit();
+                    getSupportActionBar().setTitle(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    fragment = new DashboardFragment();
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container_layout,fragment)
+                            .commit();
+                    getSupportActionBar().setTitle(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    fragment = new NotificationsFragment();
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.container_layout,fragment)
+                            .commit();
+                    getSupportActionBar().setTitle(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -35,8 +49,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
+
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        if(savedInstanceState == null){
+            navView.setSelectedItemId(R.id.navigation_home);
+        }
     }
 
 }
