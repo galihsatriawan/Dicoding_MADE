5d69ab1a932e2e77cef98d17574ccf0cece7b749
galihsatriawan <satriawan.galih99@gmail.com>
2019-07-29 09:14:34 +0700

    - Service - Bound Service

---
+++ MyService/app/src/main/java/id/shobrun/myservice/BoundService.java
@@ -0,0 +1,66 @@
+package id.shobrun.myservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.CountDownTimer;
+import android.os.IBinder;
+import android.util.Log;
+
+public class BoundService extends Service {
+    final String TAG = BoundService.class.getSimpleName();
+    MyBinder mBinder = new MyBinder();
+    final long startTime = System.currentTimeMillis();
+    public BoundService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate: ");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d(TAG, "onBind: ");
+        mTimer.start();
+        return mBinder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.d(TAG, "onUnbind: ");
+        mTimer.cancel();
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        super.onRebind(intent);
+        Log.d(TAG, "onRebind: ");
+    }
+
+    class MyBinder extends Binder {
+        BoundService getService(){
+            return BoundService.this;
+        }
+    }
+    CountDownTimer mTimer = new CountDownTimer(100000,1000) {
+        @Override
+        public void onTick(long millisUntilFinished) {
+            long elapsedTime = System.currentTimeMillis()- startTime;
+            Log.d(TAG, "onTick: "+elapsedTime);
+        }
+
+        @Override
+        public void onFinish() {
+
+        }
+    };
+}
--- MyService/app/src/main/java/id/shobrun/myservice/MainActivity.java
+++ MyService/app/src/main/java/id/shobrun/myservice/MainActivity.java
@@ -1,6 +1,9 @@
 package id.shobrun.myservice;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +14,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button btnStartIntentService;
     Button btnStartBoundService;
     Button btnStopBoundService;
+    boolean mServiceBound = false;
+    BoundService mBoundService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,18 +42,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Intent mStartServiceIntent = new Intent(MainActivity.this,OriginService.class);
                 startService(mStartServiceIntent);
                 break;
-            case R.id.btn_start_bound_service:
-
-                break;
             case R.id.btn_start_intent_service:
                 Intent mStartIntentService = new Intent(MainActivity.this,DicodingIntentService.class);
                 mStartIntentService.putExtra(DicodingIntentService.EXTRA_DURATION,5000);
                 startService(mStartIntentService);
                 break;
+            case R.id.btn_start_bound_service:
+                Intent mBoundServiceIntent = new Intent(MainActivity.this,BoundService.class);
+                bindService(mBoundServiceIntent,mServiceConnection,BIND_AUTO_CREATE);
+                break;
             case R.id.btn_stop_bound_service:
-
+                unbindService(mServiceConnection);
                 break;
 
+        }
+    }
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            BoundService.MyBinder myBinder = (BoundService.MyBinder) service;
+            mBoundService = myBinder.getService();
+            mServiceBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mServiceBound = false;
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mServiceBound){
+            unbindService(mServiceConnection);
         }
     }
 }
