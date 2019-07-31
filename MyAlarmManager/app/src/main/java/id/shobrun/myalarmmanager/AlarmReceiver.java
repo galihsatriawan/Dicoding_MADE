b55ed3877458d6ccf1e2a111ae4c8d868f62df97
galihsatriawan <satriawan.galih99@gmail.com>
2019-07-31 10:12:05 +0700

    - Alarm Manager - Method Show Notification

--- MyAlarmManager/app/src/main/java/id/shobrun/myalarmmanager/AlarmReceiver.java
+++ MyAlarmManager/app/src/main/java/id/shobrun/myalarmmanager/AlarmReceiver.java
@@ -1,11 +1,19 @@
 package id.shobrun.myalarmmanager;
 
 import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -78,6 +86,38 @@ public class AlarmReceiver extends BroadcastReceiver {
             return false;
         }catch(ParseException e) {
             return true;
+        }
+    }
+    private void showAlarmNotification(Context context, String title, String message, int notifId){
+        String CHANNEL_ID = "Channel_1";
+        String CHANNEL_NAME = "AlarmManager channel";
+
+        NotificationManager notificationManagerCompat = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context,CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_access_time_black_24dp)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setColor(ContextCompat.getColor(context,android.R.color.transparent))
+                .setVibrate(new long[]{1000,1000,1000,1000,1000})
+                .setSound(alarmSound);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
+                    CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            channel.enableVibration(true);
+            channel.setVibrationPattern(new long[]{1000,1000,1000,1000,1000});
+
+            builder.setChannelId(CHANNEL_ID);
+
+            if(notificationManagerCompat != null){
+                notificationManagerCompat.createNotificationChannel(channel);
+            }
+        }
+        Notification notification = builder.build();
+        if(notificationManagerCompat != null){
+            notificationManagerCompat.notify(notifId,notification);
         }
     }
 }
