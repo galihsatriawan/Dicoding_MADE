ce1747aedf60accbeb0f004e9292337910710192
galihsatriawan <satriawan.galih99@gmail.com>
2019-07-31 09:15:47 +0700

    - Broadcast Receiver - Download Service

---
+++ app/src/main/java/id/shobrun/smslistenerapp/DownloadService.java
@@ -0,0 +1,36 @@
+package id.shobrun.smslistenerapp;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ * TODO: Customize class - update intent actions and extra parameters.
+ */
+public class DownloadService extends IntentService {
+    public static final String TAG = "DownloadService";
+
+    public DownloadService() {
+        super("DownloadService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d(TAG, "onHandleIntent: ");
+        if (intent != null) {
+            try{
+                Thread.sleep(5000);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            Intent notifyFinishIntent = new Intent(MainActivity.ACTION_DOWNLOAD_STATUS);
+            sendBroadcast(notifyFinishIntent);
+        }
+    }
+
+
+}
